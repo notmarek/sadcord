a287e6d394dc6351122abbd80b9141c639c68541
Marek Veselý <veselym@spstrutnov.cz>
2022-10-05 23:01:34 +0200

    please

--- src/mainWindow.js
+++ src/mainWindow.js
@@ -64,98 +64,111 @@ const themesync = async () => {
     if (value !== pastValue)
         DiscordNative.userDataCache.cacheUserData(JSON.stringify(cached));
 };
-var FluxDispatcher = findModule("_dispatcher")._dispatcher;
 
-var applyPatches = () => {
-  findModule("getCurrentUser").getCurrentUser().premiumType = 2;
+let interval = setInterval(() => {
+    try {
+        var FluxDispatcher = findModule("_dispatcher")._dispatcher;
 
-  let wpRequire;
-  window.webpackChunkdiscord_app.push([
-      [Math.random()],
-      {},
-      (e) => {
-          wpRequire = e;
-      },
-  ]),
-      (mod = Object.values(wpRequire.c).find(
-          (e) => void 0 !== e?.exports?.Z?.isDeveloper
-      )),
-      (usermod = Object.values(wpRequire.c).find(
-          (e) => e?.exports?.default?.getUsers
-      )),
-      (nodes = Object.values(
-          mod.exports.Z._dispatcher._actionHandlers._dependencyGraph.nodes
-      ));
-  try {
-      nodes
-          .find((e) => "ExperimentStore" == e.name)
-          .actionHandler.OVERLAY_INITIALIZE({ user: { flags: 1 } });
-  } catch (e) {}
-  (oldGetUser = usermod.exports.default.__proto__.getCurrentUser),
-      (usermod.exports.default.__proto__.getCurrentUser = () => ({
-          hasFlag: () => !0,
-      })),
-      nodes
-          .find((e) => "DeveloperExperimentStore" == e.name)
-          .actionHandler.CONNECTION_OPEN(),
-      (usermod.exports.default.__proto__.getCurrentUser = oldGetUser);
+        var applyPatches = () => {
+            findModule("getCurrentUser").getCurrentUser().premiumType = 2;
 
-  console.log("%cSadCord Activated!", "font-size: 45px");
-  console.log("%cExperiments Enabled!", "font-size: 30px");
-  console.log("%cNitro Spoofed!", "font-size: 30px");
-}
+            let wpRequire;
+            window.webpackChunkdiscord_app.push([
+                [Math.random()],
+                {},
+                (e) => {
+                    wpRequire = e;
+                },
+            ]),
+                (mod = Object.values(wpRequire.c).find(
+                    (e) => void 0 !== e?.exports?.Z?.isDeveloper
+                )),
+                (usermod = Object.values(wpRequire.c).find(
+                    (e) => e?.exports?.default?.getUsers
+                )),
+                (nodes = Object.values(
+                    mod.exports.Z._dispatcher._actionHandlers._dependencyGraph
+                        .nodes
+                ));
+            try {
+                nodes
+                    .find((e) => "ExperimentStore" == e.name)
+                    .actionHandler.OVERLAY_INITIALIZE({ user: { flags: 1 } });
+            } catch (e) {}
+            (oldGetUser = usermod.exports.default.__proto__.getCurrentUser),
+                (usermod.exports.default.__proto__.getCurrentUser = () => ({
+                    hasFlag: () => !0,
+                })),
+                nodes
+                    .find((e) => "DeveloperExperimentStore" == e.name)
+                    .actionHandler.CONNECTION_OPEN(),
+                (usermod.exports.default.__proto__.getCurrentUser = oldGetUser);
 
-applyPatches();
-FluxDispatcher.setInterceptor((e)=>{
-  if (window.edit_logger && window.delete_logger) {
-    FluxDispatcher.setInterceptor(() => {});
-  }
-  if (!window.edit_logger && e.type === "MESSAGE_UPDATE") {
-    FluxDispatcher._actionHandlers._orderedActionHandlers.MESSAGE_UPDATE.find(
-        (h) => h.name === "MessageStore"
-    ).actionHandler = (args) => {
-        try {
-            const messageContent = document.querySelector(
-                `#message-content-${args.message.id}`
+            console.log("%cSadCord Activated!", "font-size: 45px");
+            console.log("%cExperiments Enabled!", "font-size: 30px");
+            console.log("%cNitro Spoofed!", "font-size: 30px");
+        };
+
+        applyPatches();
+        FluxDispatcher.setInterceptor((e) => {
+            if (window.edit_logger && window.delete_logger) {
+                FluxDispatcher.setInterceptor(() => {});
+            }
+            if (!window.edit_logger && e.type === "MESSAGE_UPDATE") {
+                FluxDispatcher._actionHandlers._orderedActionHandlers.MESSAGE_UPDATE.find(
+                    (h) => h.name === "MessageStore"
+                ).actionHandler = (args) => {
+                    try {
+                        const messageContent = document.querySelector(
+                            `#message-content-${args.message.id}`
+                        );
+                        const messageContainer = document.querySelector(
+                            `#chat-messages-${args.message.id}`
+                        );
+                        const buttonContainer = messageContainer.querySelector(
+                            ".buttonContainer-1502pf"
+                        );
+                        messageContainer.style = "background-color: black;";
+                        buttonContainer.prepend("(edited)");
+                        buttonContainer.style = "color: white;";
+                        messageContent.innerText += "\n" + args.message.content;
+                    } catch {}
+                };
+                window.edit_logger = true;
+                console.log(
+                    "%cMessage Edit Logger Enabled!",
+                    "font-size: 30px"
+                );
+            } else if (!window.delete_logger && e.type === "MESSAGE_DELETE") {
+                FluxDispatcher._actionHandlers._orderedActionHandlers.MESSAGE_DELETE.find(
+                    (h) => h.name === "MessageStore"
+                ).actionHandler = (args) => {
+                    try {
+                        document.querySelector(
+                            `#chat-messages-${args.id}`
+                        ).style = "background-color: black;";
+                        document.querySelector(
+                            `#message-content-${args.id}`
+                        ).style = "color: red;";
+                    } catch {}
+                };
+                window.delete_logger = true;
+                console.log(
+                    "%cMessage Delete Logger Enabled!",
+                    "font-size: 30px"
+                );
+            }
+        });
+        FluxDispatcher.subscribe("CONNECTION_OPEN", (e) => {
+            console.log(
+                `%cLogged in as ${e.user.username}#${e.user.discriminator}!`,
+                "font-size: 30px"
             );
-            const messageContainer = document.querySelector(
-                `#chat-messages-${args.message.id}`
-            );
-            const buttonContainer = messageContainer.querySelector(
-                ".buttonContainer-1502pf"
-            );
-            messageContainer.style = "background-color: black;";
-            buttonContainer.prepend("(edited)");
-            buttonContainer.style = "color: white;";
-            messageContent.innerText += "\n" + args.message.content;
-        } catch {}
-        window.edit_logger = true;
-        console.log("%cMessage Edit Logger Enabled!", "font-size: 30px");
-
-    };
-  } else if (!window.delete_logger && e.type === "MESSAGE_DELETE") {
-    FluxDispatcher._actionHandlers._orderedActionHandlers.MESSAGE_DELETE.find(
-        (h) => h.name === "MessageStore"
-    ).actionHandler = (args) => {
-        try {
-            document.querySelector(`#chat-messages-${args.id}`).style =
-                "background-color: black;";
-            document.querySelector(`#message-content-${args.id}`).style =
-                "color: red;";
-        } catch {}
-    };
-    window.delete_logger = true;
-    console.log("%cMessage Delete Logger Enabled!", "font-size: 30px");
-  }
-});
-findModule("_dispatcher")._dispatcher.subscribe("CONNECTION_OPEN", (e) => {
-  console.log(
-      `%cLogged in as ${e.user.username}#${e.user.discriminator}!`,
-      "font-size: 30px"
-  );
-  findModule("getCurrentUser").getCurrentUser().premiumType = 2;
-
-});
+            findModule("getCurrentUser").getCurrentUser().premiumType = 2;
+        });
+        clearInterval(interval);
+    } catch {}
+}, 1000);
 
 // Settings info version injection
 setInterval(() => {
